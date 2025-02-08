0623d36f787321064cf656f52de9c0c61140b6fd
ADAM <2201020027@universitasbumigora.ac.id>
2025-02-09 06:47:10 +0800

    Import font Inter

---
+++ src/app/layout.jsx
@@ -0,0 +1,21 @@
+import Navbar from "@/components/Navbar";
+import "../app/globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="bg-primary _monica-theme-d2f2b383">
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+      </body>
+    </html>
+  );
+}
