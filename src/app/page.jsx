f22382053f09a6b67d6d1b52729467cfd3f2102f
ADAM <2201020027@universitasbumigora.ac.id>
2025-02-09 06:47:31 +0800

    Add Biography

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Biography from '@/components/Biography';
+import Hero from '../components/hero';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Biography />
+    </>
+  );
+}
