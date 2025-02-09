32e0cc045139b3d7e366b52141d34c9831825242
ADAM <2201020027@universitasbumigora.ac.id>
2025-02-09 10:49:21 +0800

    Uppdate Title and Add Icons

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>Bayani Adam Sasaki</title>
+        <link rel="icon" type="image/svg" href="/logo.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
