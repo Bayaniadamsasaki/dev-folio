68b2942dc818cbcd3c1ec5608eb0c6331bb62fe8
ADAM <2201020027@universitasbumigora.ac.id>
2025-02-19 19:48:05 +0800

    Data Update Local

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,5 +1,5 @@
 const Data = {
-  hamburger: "http://localhost:3000/menu.svg",
+  hamburger: "/menu.svg",
   menuItems: [
     { name: "Home", link: "#home" },
     { name: "Biography", link: "#biography" },
@@ -9,28 +9,28 @@ const Data = {
   ],
 
   vectorHero: [
-    { src: "http://localhost:3000/vector-1.svg", alt: "Vector 1" },
-    { src: "http://localhost:3000/vector-2.svg", alt: "Vector 2" },
-    { src: "http://localhost:3000/vector-3.svg", alt: "Vector 3" },
+    { src: "/vector-1.svg", alt: "Vector 1" },
+    { src: "/vector-2.svg", alt: "Vector 2" },
+    { src: "/vector-3.svg", alt: "Vector 3" },
   ],
 
   iconCards: [
     {
       title: "Design Thinking",
-      icon: "http://localhost:3000/figma.svg",
+      icon: "/figma.svg",
       year: "2023",
       alt: "Figma",
     },
     {
       title: "Clean Code",
-      icon: "http://localhost:3000/react.svg",
+      icon: "/react.svg",
       year: "2024",
       alt: "React",
     },
   ],
 
   Biography: [
-    { profile: "http://localhost:3000/profile.png", alt: "profil-adam" },
+    { profile: "/profile.png", alt: "profil-adam" },
   ],
 
   linkSocial: [
